15628b625c86d96f8eb44d31114175416c25ee65
samantaProspero <samantaprospero@gmail.com>
2020-07-16 11:50:56 -0300

    delete db

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const {Pool} = require("pg")
 
 module.exports = new Pool({
     user:'postgres',
-    password: "capota",
+    password: "",
     host: "localhost",
     port: 5432,
     database:"myTeacher"
